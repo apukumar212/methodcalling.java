8a2d1057ca3bfd39b93198b4394937b2ec7ca959
Apu kumar Giri <87590236+apukumar212@users.noreply.github.com>
2022-11-22 19:24:00 +0530

    Add files via upload

---
+++ ClassF.java
@@ -0,0 +1,39 @@
+package com.task1;
+
+public class ClassF 
+{
+void meth1()
+{
+	System.out.println("JAVA");
+	System.out.println(" IS ");
+	System.out.println("AWESOME");
+}
+void meth2()
+{
+	System.out.print("java");
+	System.out.print(" is ");
+	System.out.println("awesome");
+}
+void meth3()
+{
+	System.out.print("java");
+	System.out.print(" is ");
+	System.out.println("awesome");
+	System.out.print("Hello");
+}
+public static void main(String[] args)
+{
+ClassF fobj	=new ClassF();
+fobj.meth1();
+System.out.println("------------------");
+fobj.meth2();
+System.out.println("-------------------");
+fobj.meth3();
+}
+}
+
+
+
+
+
+
